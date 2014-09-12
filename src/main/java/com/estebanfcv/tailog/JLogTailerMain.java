6665dcefc92dc8a92017d7c371f83bad4e876e9e
estebanfcv <estebanfcv@gmail.com>
2014-09-11 21:58:32 -0500

    se sube le código

---
+++ src/main/java/com/estebanfcv/tailog/JLogTailerMain.java
@@ -0,0 +1,13 @@
+package com.estebanfcv.tailog;
+
+/**
+ * 
+ * @author estebanfcv
+ */
+public class JLogTailerMain {
+    
+    public static void main(String[] args) {
+        new JLogTailerFrame("JLogTailer", 640, 480);
+    }
+    
+}
